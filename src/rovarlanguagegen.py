8929f86b961245941b7b773d8ca839cf222da79f
Carl Raunås <carl.raunas@svenskaspel.se>
2020-02-04 10:35:22 +0100

    added stuff

--- src/rovarlanguagegen.py
+++ src/rovarlanguagegen.py
@@ -1,5 +1,9 @@
+#importerar test.py som ligger i samma dir - kolla där
 import test
 
+#här anropas funktionen test, som definierats i test.py, med parameter = 'asd'
 test.test('asd')
-print("hejhej")
-print ("Många tack för tårtan")
+
+#steppar igenom en början på hur vi skulle kunna lösa rovarspraket stuff
+#1 googla hur man skapar en lista i python. 
+
--- src/test.py
+++ src/test.py
@@ -1,2 +1,3 @@
+#här definieras en funktion som skriver ut parametern val. Vi kan gå igenom det
 def test(val):
     print(val)
